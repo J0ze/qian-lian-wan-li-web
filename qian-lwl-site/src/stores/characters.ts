07fca31a0970822446aa984747ad0f99a9d39296
J0ze <3431551044@qq.com>
2025-10-09 09:28:34 +0800

    优化图片存储格式

--- qian-lwl-site/src/stores/characters.ts
+++ qian-lwl-site/src/stores/characters.ts
@@ -18,7 +18,7 @@ export const useCharacterStore = defineStore('characters', {
         role: '机智可爱的二次元少女',
         description: '青蓝计划理论研究者后代，DUT与MIT的交换生，对元宇宙专业知识了解通透，属于二次元里面万事通那一类，机智且可爱。',
         outfits: [
-          new URL('/src/assets/characters/xingqiao.jpg', import.meta.url).href,
+          new URL('/src/assets/characters/xingqiao/xingqiao.jpg', import.meta.url).href,
         ],
         currentOutfitIndex: 0
       },
@@ -28,7 +28,7 @@ export const useCharacterStore = defineStore('characters', {
         role: '体育天赋超群的幼驯染',
         description: '青蓝计划投资者后代，蒋忱的幼驯染，体育天赋超群，小时候以欺负蒋忱为乐，活泼俏皮（富，给蒋忱氪金。分数不够但为了黏蒋忱上了盘锦的中外合办',
         outfits: [
-          new URL('/src/assets/characters/ling.jpg', import.meta.url).href,
+          new URL('/src/assets/characters/ling/ling.jpg', import.meta.url).href,
         ],
         currentOutfitIndex: 0
       },
@@ -38,7 +38,7 @@ export const useCharacterStore = defineStore('characters', {
         role: '温柔沉稳的学姐',
         description: '青蓝计划设备维护者后代，温柔沉稳的学姐，托这份关系让蒋忱取得了科研机会（喜欢在句尾加“差不多酱紫',
         outfits: [
-          new URL('/src/assets/characters/ye.jpg', import.meta.url).href,
+          new URL('/src/assets/characters/ye/ye.jpg', import.meta.url).href,
         ],
         currentOutfitIndex: 0
       },
